db174940518574253a5d7c82be8b17cc6f4ab493
Waitupcali <jihyeok2yo@gmail.com>
2020-04-08 20:47:55 +0900

    third commit

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "rooms"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.RoomsView.as_view()),
+    path("search/", views.room_search),
+    path("<int:pk>/", views.RoomView.as_view()),
+]
